224fa4f3c3a60ad29c35135367458dd816edad11
Gyeongmin Lee <gminlee.316@gmail.com>
2023-02-13 00:06:31 -0800

    part 2-c

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,10 +1,18 @@
 import Note from "./components/Note";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-const App = ({ notes: propNotes }) => {
-  const [notes, setNotes] = useState(propNotes);
+const App = () => {
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/notes").then((response) => {
+      console.log("promise fulfilled");
+      setNotes(response.data);
+    });
+  }, []);
 
   const notesToShow = showAll
     ? notes
