31e5c71619f7d8b8c34f9176778879e3f1040552
Gyeongmin Lee <gminlee.316@gmail.com>
2023-02-12 23:05:10 -0800

    part 2 notes

---
+++ part2/notes/src/App.js
@@ -0,0 +1,53 @@
+import Note from "./components/Note";
+import { useState } from "react";
+
+const App = ({ notes: propNotes }) => {
+  const [notes, setNotes] = useState(propNotes);
+  const [newNote, setNewNote] = useState("");
+  const [showAll, setShowAll] = useState(true);
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter((note) => note.important === true);
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    };
+
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
+  };
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
+      <ul>
+        <ul>
+          {notesToShow.map((note) => (
+            <Note key={note.id} note={note} />
+          ))}
+        </ul>
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
