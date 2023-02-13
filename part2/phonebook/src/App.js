218788d0ce7f9dfa481996ef2ed5781ea5fccb32
Gyeongmin Lee <gminlee.316@gmail.com>
2023-02-12 23:29:51 -0800

    Exercises 2.5.-2.10.

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import Filter from "./components/Filter";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
+
+  const addPerson = (event) => {
+    event.preventDefault();
+
+    const isPersonExist = persons.some((person) => person.name === newName);
+    if (isPersonExist) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
+
+    setPersons([...persons, { name: newName, number: newNumber }]);
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  const filteredPersons = persons.filter((person) => {
+    const lowerCaseName = person.name.toLowerCase();
+    const lowerCaseFilter = filter.toLowerCase();
+    return lowerCaseName.includes(lowerCaseFilter);
+  });
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter value={filter} onChange={handleFilterChange} />
+      <h2>Add a new</h2>
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+      <h2>Numbers</h2>
+      <Persons persons={filteredPersons} />
+    </div>
+  );
+};
+
+export default App;
