738f346260cc6904c6b7b357a47f6a704d54b9fc
Gyeongmin Lee <gminlee.316@gmail.com>
2023-02-15 21:24:03 -0800

    Part 3 b

--- part3/note_backend/index.js
+++ part3/note_backend/index.js
@@ -1,7 +1,10 @@
+const cors = require("cors");
 const express = require("express");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+app.use(express.static("build"));
 
 let notes = [
   {
@@ -73,7 +76,7 @@ app.post("/api/notes", (request, response) => {
   response.json(note);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
