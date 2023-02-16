99e8a080933febcd734dbdec0e9f74795cc2a6f1
Gyeongmin Lee <gminlee.316@gmail.com>
2023-02-15 21:39:37 -0800

    Exercises 3.9.-3.11.

---
+++ part3/phonebook_backend/README.md
@@ -0,0 +1,5 @@
+# Phonebook Backend
+
+This is the backend for the phonebook application. It is a REST API that can be used to store and retrieve phonebook entries.
+
+[LIVE DEMO](https://proud-flower-2205.fly.dev/)
--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -10,6 +10,7 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :data")
 );
 app.use(express.json());
+app.use(express.static("build"));
 
 let persons = [
   {
@@ -105,7 +106,7 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
