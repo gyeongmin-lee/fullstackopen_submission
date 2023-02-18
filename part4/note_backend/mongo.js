6d8889a003867abb904034bdaa168825c9252d38
Gyeongmin Lee <gminlee.316@gmail.com>
2023-02-17 21:59:24 -0800

    Part 4 testing the backend

--- part4/note_backend/mongo.js
+++ part4/note_backend/mongo.js
@@ -1,42 +1,31 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-if (process.argv.length<3) {
-  console.log('give password as argument')
-  process.exit(1)
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
 }
 
-const password = process.argv[2]
+const password = process.argv[2];
 
-const url =
-  `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/?retryWrites=true&w=majority`
+const url = `mongodb+srv://gminlee316:${password}@cluster0.kwybjz8.mongodb.net/testNoteApp?retryWrites=true&w=majority`;
 
-mongoose.set('strictQuery',false)
-mongoose.connect(url)
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
 
 const noteSchema = new mongoose.Schema({
   content: String,
   date: Date,
   important: Boolean,
-})
+});
 
-const Note = mongoose.model('Note', noteSchema)
+const Note = mongoose.model("Note", noteSchema);
 
 const note = new Note({
-  content: 'Mongoose makes things easy',
-  date: new Date(),
+  content: "MongoDB is so cool!",
   important: true,
-})
+});
 
-/*
-note.save().then(result => {
-  console.log('note saved!')
-  mongoose.connection.close()
-})
-*/
-
-Note.find({}).then(result => {
-  result.forEach(note => {
-    console.log(note)
-  })
-  mongoose.connection.close()
-})
+note.save().then((result) => {
+  console.log("note saved!");
+  mongoose.connection.close();
+});
