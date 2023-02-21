86b53fe50b6cac06443598064364da8987a77cff
Gyeongmin Lee <gminlee.316@gmail.com>
2023-02-21 11:25:06 -0800

    Exercises 4.22.-23.

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 app.use(middleware.tokenExtractor);
 
-app.use("/api/blogs", blogRouter);
+app.use("/api/blogs", middleware.userExtractor, blogRouter);
 app.use("/api/users", userRouter);
 app.use("/api/login", loginRouter);
 
