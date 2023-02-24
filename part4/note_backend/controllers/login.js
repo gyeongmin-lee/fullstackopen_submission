2a128cfd2ebed0d67ea84dfab94a41c4140aa4fd
Gyeongmin Lee <gminlee.316@gmail.com>
2023-02-24 10:54:29 -0800

    change expiry value

--- part4/note_backend/controllers/login.js
+++ part4/note_backend/controllers/login.js
@@ -23,7 +23,7 @@ loginRouter.post("/", async (request, response) => {
   };
 
   const token = jwt.sign(userForToken, process.env.SECRET, {
-    expiresIn: 60 * 60,
+    expiresIn: "1h",
   });
 
   response
