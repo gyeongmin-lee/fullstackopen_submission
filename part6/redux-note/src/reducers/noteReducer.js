cfd78da9e0e4ba1286840ced8cb6334d498057f2
Gyeongmin Lee <gminlee.316@gmail.com>
2023-02-27 22:28:25 -0800

    Part 6 b

--- part6/redux-note/src/reducers/noteReducer.js
+++ part6/redux-note/src/reducers/noteReducer.js
@@ -1,3 +1,5 @@
+import { createSlice } from "@reduxjs/toolkit";
+
 const generateId = () => Number((Math.random() * 1000000).toFixed(0));
 
 const initialState = [
@@ -13,40 +15,29 @@ const initialState = [
   },
 ];
 
-export const createNote = (content) => {
-  return {
-    type: "NEW_NOTE",
-    payload: {
-      content,
-      important: false,
-      id: generateId(),
+const noteSlice = createSlice({
+  name: "notes",
+  initialState,
+  reducers: {
+    createNote(state, action) {
+      const content = action.payload;
+      state.push({
+        content,
+        important: false,
+        id: generateId(),
+      });
     },
-  };
-};
-
-export const toggleImportanceOf = (id) => {
-  return {
-    type: "TOGGLE_IMPORTANCE",
-    payload: { id },
-  };
-};
-
-const noteReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "NEW_NOTE":
-      return state.concat(action.payload);
-    case "TOGGLE_IMPORTANCE": {
-      const id = action.payload.id;
+    toggleImportanceOf(state, action) {
+      const id = action.payload;
       const noteToChange = state.find((n) => n.id === id);
       const changedNote = {
         ...noteToChange,
         important: !noteToChange.important,
       };
       return state.map((note) => (note.id !== id ? note : changedNote));
-    }
-    default:
-      return state;
-  }
-};
+    },
+  },
+});
 
-export default noteReducer;
+export const { createNote, toggleImportanceOf } = noteSlice.actions;
+export default noteSlice.reducer;
