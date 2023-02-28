9bb6f54f80a98c34915977130640c162149ed7a7
Gyeongmin Lee <gminlee.316@gmail.com>
2023-02-28 10:43:41 -0800

    Part 6 c

--- part6/redux-note/src/index.js
+++ part6/redux-note/src/index.js
@@ -1,18 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import App from "./App";
-
-import noteReducer from "./reducers/noteReducer";
-import filterReducer from "./reducers/filterReducer";
-
-const store = configureStore({
-  reducer: {
-    notes: noteReducer,
-    filter: filterReducer,
-  },
-});
+import store from "./store";
 
 /*
 ReactDOM.createRoot(document.getElementById('root')).render(
