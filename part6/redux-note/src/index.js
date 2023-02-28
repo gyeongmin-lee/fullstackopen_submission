1b3f0ce7faa17bdaa0e92e7e45f9d31ce989ec95
Gyeongmin Lee <gminlee.316@gmail.com>
2023-02-27 21:54:53 -0800

    Part 6 b

--- part6/redux-note/src/index.js
+++ part6/redux-note/src/index.js
@@ -1,12 +1,27 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
-
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
 import App from "./App";
 
-import { createStore } from "redux";
 import noteReducer from "./reducers/noteReducer";
-import { Provider } from "react-redux";
+import filterReducer from "./reducers/filterReducer";
 
-const store = createStore(noteReducer);
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer,
+});
+
+const store = createStore(reducer);
+
+console.log(store.getState());
+
+/*
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)*/
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
--- part6/redux-note/src/reducers/noteReducer.js
+++ part6/redux-note/src/reducers/noteReducer.js
@@ -1,5 +1,18 @@
 const generateId = () => Number((Math.random() * 1000000).toFixed(0));
 
+const initialState = [
+  {
+    content: "reducer defines how redux store works",
+    important: true,
+    id: 1,
+  },
+  {
+    content: "state of store can contain any data",
+    important: false,
+    id: 2,
+  },
+];
+
 export const createNote = (content) => {
   return {
     type: "NEW_NOTE",
@@ -18,7 +31,7 @@ export const toggleImportanceOf = (id) => {
   };
 };
 
-const noteReducer = (state = [], action) => {
+const noteReducer = (state = initialState, action) => {
   switch (action.type) {
     case "NEW_NOTE":
       return state.concat(action.payload);
