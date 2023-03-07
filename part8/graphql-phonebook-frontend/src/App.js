13a28a2b4e0f0d02edb2fb1c0fdb58dae1a0a4a1
Gyeongmin Lee <gminlee.316@gmail.com>
2023-03-06 16:23:41 -0800

    Part 8 b

---
+++ part8/graphql-phonebook-frontend/src/App.js
@@ -0,0 +1,41 @@
+import { useQuery } from "@apollo/client";
+import { useState } from "react";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
+import PhoneForm from "./components/PhoneForm";
+import { ALL_PERSONS } from "./queries";
+
+const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const result = useQuery(ALL_PERSONS);
+
+  if (result.loading) {
+    return <div>loading...</div>;
+  }
+
+  const notify = (message) => {
+    setErrorMessage(message);
+    setTimeout(() => {
+      setErrorMessage(null);
+    }, 10000);
+  };
+
+  return (
+    <div>
+      <Notify errorMessage={errorMessage} />
+      <Persons persons={result.data.allPersons} />
+      <PersonForm setError={notify} />
+      <PhoneForm />
+    </div>
+  );
+};
+
+const Notify = ({ errorMessage }) => {
+  if (!errorMessage) {
+    return null;
+  }
+  return <div style={{ color: "red" }}>{errorMessage}</div>;
+};
+
+export default App;
