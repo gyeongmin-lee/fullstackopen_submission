f605f2976e206041e935bf99531cc70b0f0ef4d0
Gyeongmin Lee <gminlee.316@gmail.com>
2023-03-10 19:52:13 -0800

    Part 9 b adding eslint

--- part9/typescript-practice/index.ts
+++ part9/typescript-practice/index.ts
@@ -1,8 +1,21 @@
 import express from "express";
+import { calculator, Operation } from "./calculator";
 const app = express();
 
 app.get("/ping", (_req, res) => {
   res.send("pong");
+});
+
+app.post("/calculate", (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { value1, value2, op } = req.body;
+
+  if (!value1 || isNaN(Number(value1))) {
+    return res.status(400).send({ error: "..." });
+  }
+
+  const result = calculator(Number(value1), Number(value2), op as Operation);
+  return res.send({ result });
 });
 
 const PORT = 3003;
