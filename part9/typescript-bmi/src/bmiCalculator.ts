c83118ef0980aaf1e9d401930c52037281c7c56d
Gyeongmin Lee <gminlee.316@gmail.com>
2023-03-10 17:28:41 -0800

    Exercises 9.1.-9.3.

---
+++ part9/typescript-bmi/src/bmiCalculator.ts
@@ -0,0 +1,47 @@
+type BmiValues = {
+  height: number;
+  weight: number;
+};
+
+type Bmi = "Normal (healthy weight)" | "Underweight" | "Overweight" | "Obese";
+
+const calculateBmi = (height: number, weight: number): Bmi => {
+  const bmi = weight / ((height / 100) * (height / 100));
+
+  if (bmi < 18.5) {
+    return "Underweight";
+  } else if (bmi < 25) {
+    return "Normal (healthy weight)";
+  } else if (bmi < 30) {
+    return "Overweight";
+  } else {
+    return "Obese";
+  }
+};
+
+const parseArguments = (args: string[]): BmiValues => {
+  if (args.length < 4) throw new Error("Not enough arguments");
+  if (args.length > 4) throw new Error("Too many arguments");
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3]),
+    };
+  } else {
+    throw new Error("Provided values were not numbers!");
+  }
+};
+
+try {
+  const { height, weight } = parseArguments(process.argv);
+  console.log(calculateBmi(height, weight));
+} catch (error: unknown) {
+  let errorMessage = "Something bad happened.";
+  if (error instanceof Error) {
+    errorMessage += " Error: " + error.message;
+  }
+  console.log(errorMessage);
+}
+
+export {};
---
+++ part9/typescript-bmi/src/exerciseCalculator.ts
@@ -0,0 +1,79 @@
+interface ExerciseResult {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+const calculateExercises = (
+  exerciseData: number[],
+  targetDailyHours: number
+): ExerciseResult => {
+  const periodLength = exerciseData.length;
+  const trainingDays = exerciseData.filter((hours) => hours > 0).length;
+
+  const totalHours = exerciseData.reduce((a, b) => a + b, 0);
+  const average = totalHours / periodLength;
+
+  const success = average >= targetDailyHours;
+
+  let rating = 1;
+  let ratingDescription = "You can do better!";
+
+  if (average >= targetDailyHours * 1.5) {
+    rating = 3;
+    ratingDescription = "You're doing great!";
+  }
+
+  if (average >= targetDailyHours && average < targetDailyHours * 1.5) {
+    rating = 2;
+    ratingDescription = "Not too bad but could be better";
+  }
+
+  return {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target: targetDailyHours,
+    average,
+  };
+};
+
+interface ExerciseValues {
+  exerciseData: number[];
+  target: number;
+}
+
+const parseArguments = (args: string[]): ExerciseValues => {
+  if (args.length < 4) throw new Error("Not enough arguments");
+
+  const target = Number(args[2]);
+  const exerciseData = args.slice(3).map((hours) => Number(hours));
+
+  if (isNaN(target) || exerciseData.some((hours) => isNaN(hours))) {
+    throw new Error("Provided values were not numbers!");
+  }
+
+  return {
+    exerciseData,
+    target,
+  };
+};
+
+try {
+  const { exerciseData, target } = parseArguments(process.argv);
+  console.log(calculateExercises(exerciseData, target));
+} catch (error: unknown) {
+  let errorMessage = "Something bad happened.";
+  if (error instanceof Error) {
+    errorMessage += " Error: " + error.message;
+  }
+  console.log(errorMessage);
+}
+
+export {};
