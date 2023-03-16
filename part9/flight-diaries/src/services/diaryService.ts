424ed30dc539437115507d4efa257a5debba5d2a
Gyeongmin Lee <gminlee.316@gmail.com>
2023-03-15 20:21:30 -0700

    Part 9 c

---
+++ part9/flight-diaries/src/services/diaryService.ts
@@ -0,0 +1,25 @@
+import diaries from "../../data/entries";
+import { DiaryEntry, NonSensitiveDiaryEntry } from "../types";
+
+const getEntries = (): DiaryEntry[] => {
+  return diaries;
+};
+
+const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
+  return diaries.map(({ id, date, weather, visibility }) => ({
+    id,
+    date,
+    weather,
+    visibility,
+  }));
+};
+
+const addDiary = () => {
+  return null;
+};
+
+export default {
+  getEntries,
+  addDiary,
+  getNonSensitiveEntries,
+};
